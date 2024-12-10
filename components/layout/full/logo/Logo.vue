730057bd3bf509a41d1f7156e5194d9e1b463f88
atana <pinghai122@gmail.com>
2024-12-11 00:35:18 +0100

    del

--- components/layout/full/logo/Logo.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <LayoutFullLogoDark />
-</template>
-
--- middleware/auth.global.js
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    const { userToken } = useAuth()
-
-    if (!userToken.value && to.path !== '/auth/login' && to.path !== '/auth/register') {
-      return navigateTo('/auth/login')
-    }
-})
